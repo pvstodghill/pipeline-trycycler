a76128381526d54244132bfddf55603e43def209
Paul Stodghill <paul.stodghill@usda.gov>
2023-08-01 10:15:35 -0400

    doit13: Don't rotate linear plasmids!

---
+++ config13.template.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# The linear contigs.
+# - These contigs will not be rotated to a heuristically better origin.
+# - The names used are the ones assigned by `normalize_assembly`. So,
+#   this stage might have to be run twice.
+# - Names are comma separated with no spaces.
+
+LINEAR_CONTIGS=""
--- doit13-normalize.bash
+++ doit13-normalize.bash
@@ -2,10 +2,13 @@
 
 . $(dirname ${BASH_SOURCE[0]})/doit-preamble.bash
 
+. config13.bash
+
 # ------------------------------------------------------------------------
 
 if [ ! -f ${DATA}/assembly.fasta ] ; then
-    echo 1>&2 '${DATA}/assembly.fasta is missing.'
+    echo 1>&2 "${DATA}/assembly.fasta is missing."
+    echo 1>&2 "Perhaps: cp ${NEXTPOLISH}/polished.fasta ${DATA}/assembly.fasta"
     exit 1
 fi
 
@@ -25,6 +28,7 @@ cat ${DATA}/assembly.fasta \
 ${PIPELINE}/scripts/normalize-assembly \
     -d ${NORMALIZED}/tmp \
     -f ${PIPELINE}/inputs/starts.faa \
+    -l "$LINEAR_CONTIGS" \
     ${NORMALIZED}/unnormalized.fasta ${STRAIN}${VERSION}_ \
     > ${NORMALIZED}/normalized.fasta
 
