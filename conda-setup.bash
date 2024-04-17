8c149008e56bec14138d83c8a77f341e2ecf0458
Paul Stodghill <paul.stodghill@usda.gov>
2024-04-17 09:39:32 -0400

    Migrated from multiple containers to single pipeline- container.

--- conda-setup.bash
+++ conda-setup.bash
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-echo 1>&2 this currently fails. conflict with medaka.
-exit 1
-
-
 set -e
 
 NAME=pipeline-trycycler
+if [ "${TARGET_CONDA_ENV}" ] ; then
+    NAME="${TARGET_CONDA_ENV}"
+fi
 
 CONDA_PREFIX=$(dirname $(dirname $(type -p conda)))
 . "${CONDA_PREFIX}/etc/profile.d/conda.sh"
@@ -29,7 +28,7 @@ PACKAGES+=" minipolish"
 PACKAGES+=" mummer"
 PACKAGES+=" necat"
 PACKAGES+=" nextpolish"
-PACKAGES+=" raven"
+PACKAGES+=" raven-assembler"
 PACKAGES+=" referenceseeker"
 PACKAGES+=" samtools"
 PACKAGES+=" seqtk"
@@ -37,7 +36,6 @@ PACKAGES+=" trycycler"
 PACKAGES+=" unicycler"
 
 #PACKAGES+=" wtdbg2" - not available
-
 
 if [ "$(type -p mamba)" ] ; then
     _conda="mamba --no-banner"
@@ -53,11 +51,14 @@ function __ {
 if [ "$1" = -f ] ; then
     __ conda env remove -y --name ${NAME}
 fi
+
+_install=update
 if [ ! -d ${CONDA_PREFIX}/envs/${NAME} ] ; then
     __ conda create -y --name ${NAME}
+    _install=install
 fi
 __ conda activate ${NAME}
 
-__ $_conda install -y ${PACKAGES}
+__ $_conda $_install -y ${PACKAGES}
 
-# __ pip install FIXME
+# __ pip $_install FIXME
