be9d7c7ef4245db45099f2ce2b39abf22c42c05e
Paul Stodghill <paul.stodghill@usda.gov>
2024-04-17 09:40:33 -0400

    `trycycler subsample` was new in V0.4.0. Use it!

--- doit02-assemblies.bash
+++ doit02-assemblies.bash
@@ -9,28 +9,28 @@
 # (https://github.com/rrwick/Trycycler/wiki/Generating-assemblies-for-Trycycler)
 # ------------------------------------------------------------------------
 
-. ${FILTLONG}/stats.bash
+# ------------------------------------------------------------------------
+# Subsample reads
+# ------------------------------------------------------------------------
 
 mkdir -p ${ASSEMBLIES}/tmp ${ASSEMBLIES}/inputs
 
-if [ -z "$SEED_BASE" ] ; then
-    SEED_BASE=0
-fi
-SEED_BASE_SKIP=20
+NUM_SUBSAMPLES=0
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_CANU_ASSEMBLIES} ]
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_FLYE_ASSEMBLIES} ]
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_MINIPOLISH_ASSEMBLIES} ]
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_NECAT_ASSEMBLIES} ]
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_RAVEN_ASSEMBLIES} ]
+NUM_SUBSAMPLES=$[ ${NUM_SUBSAMPLES} + ${NUM_REDBEAN_ASSEMBLIES} ]
 
-function make_subsample {
-    tag=$1
-    j=$2
+trycycler subsample \
+	  --genome_size ${GENOME_SIZE} \
+	  --reads ${FILTLONG}/filtered_nanopore.fastq.gz \
+	  --out_dir ${ASSEMBLIES}/inputs \
+	  --count ${NUM_SUBSAMPLES} \
+	  --threads ${THREADS}
 
-    echo "## subsampling reads"
-    SEED=$[ $SEED_BASE + $j ]
-    seqtk sample -s "$SEED" ${FILTLONG}/filtered_nanopore.fastq.gz "$read_count" \
-    	| paste - - - - \
-    	| shuf \
-    	| tr '\t' '\n' \
-	| gzip > ${ASSEMBLIES}/inputs/${tag}_"$j".fastq.gz
-
-}
+CUR_SUBSAMPLE=0
 
 # ------------------------------------------------------------------------
 # canu
@@ -42,21 +42,21 @@ for j in $(seq 0 $[ $NUM_CANU_ASSEMBLIES - 1 ]) ; do
 	echo "# canu $j - already exists"
 	continue
     fi
-    echo "# canu $j"
-    make_subsample canu $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# canu $j: ${CUR_FASTQ}"
     
     echo "## running canu"
     mkdir -p ${ASSEMBLIES}/tmp/canu_${j} 
     canu -p canu -d ${ASSEMBLIES}/tmp/canu_${j} \
 		 genomeSize="$GENOME_SIZE" \
 		 -corrected -trimmed \
-		 -nanopore ${ASSEMBLIES}/inputs/canu_${j}.fastq.gz
+		 -nanopore ${CUR_FASTQ}
     cp ${ASSEMBLIES}/tmp/canu_${j}/canu.contigs.fasta \
 	   ${ASSEMBLIES}/canu_${j}.fna
 
 done
-
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
 
 # ------------------------------------------------------------------------
 # flye
@@ -68,19 +68,19 @@ for j in $(seq 0 $[ $NUM_FLYE_ASSEMBLIES - 1 ]) ; do
 	echo "# flye $j - already exists"
 	continue
     fi
-    echo "# flye $j"
-    make_subsample flye $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# flye $j: ${CUR_FASTQ}"
     
     echo "## running flye"
-    flye --nano-raw ${ASSEMBLIES}/inputs/flye_"$j".fastq.gz \
+    flye --nano-raw ${CUR_FASTQ} \
 	 --threads "$THREADS" \
 	 --out-dir ${ASSEMBLIES}/tmp/flye_${j} 
     cp ${ASSEMBLIES}/tmp/flye_${j}/assembly.fasta \
        ${ASSEMBLIES}/flye_${j}.fna
 
 done
-
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
 
 # ------------------------------------------------------------------------
 # minipolish
@@ -92,8 +92,10 @@ for j in $(seq 0 $[ $NUM_MINIPOLISH_ASSEMBLIES - 1 ]) ; do
 	echo "# minipolish $j - already exists"
 	continue
     fi
-    echo "# minipolish $j"
-    make_subsample minipolish $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# minipolish $j: ${CUR_FASTQ}"
     
     echo "## running minipolish"
 
@@ -104,18 +106,18 @@ for j in $(seq 0 $[ $NUM_MINIPOLISH_ASSEMBLIES - 1 ]) ; do
     
     # Find read overlaps with minimap2.
     minimap2 -x ava-ont -t "$THREADS" \
-	     ${ASSEMBLIES}/inputs/minipolish_${j}.fastq.gz \
-	     ${ASSEMBLIES}/inputs/minipolish_${j}.fastq.gz \
+	     ${CUR_FASTQ} \
+	     ${CUR_FASTQ} \
 	     > ${ASSEMBLIES}/tmp/minipolish_overlap_${j}.paf
     
     # Run miniasm to make an unpolished assembly.
-    miniasm -f ${ASSEMBLIES}/inputs/minipolish_${j}.fastq.gz \
+    miniasm -f ${CUR_FASTQ} \
 	    ${ASSEMBLIES}/tmp/minipolish_overlap_${j}.paf \
 	    > ${ASSEMBLIES}/tmp/minipolish_unpolished_${j}.gfa
     
     # Polish the assembly with minipolish, outputting the result to stdout.
     minipolish --threads "$THREADS" \
-	       ${ASSEMBLIES}/inputs/minipolish_${j}.fastq.gz \
+	       ${CUR_FASTQ} \
 	       ${ASSEMBLIES}/tmp/minipolish_unpolished_${j}.gfa \
 	       > ${ASSEMBLIES}/tmp/minipolish_${j}.gfa
 
@@ -127,8 +129,6 @@ for j in $(seq 0 $[ $NUM_MINIPOLISH_ASSEMBLIES - 1 ]) ; do
 
 done
 
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
-
 # ------------------------------------------------------------------------
 # raven
 # ------------------------------------------------------------------------
@@ -139,18 +139,18 @@ for j in $(seq 0 $[ $NUM_RAVEN_ASSEMBLIES - 1 ]) ; do
 	echo "# raven $j - already exists"
 	continue
     fi
-    echo "# raven $j"
-    make_subsample raven $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# raven $j: ${CUR_FASTQ}"
     
     echo "## running raven"
-    raven --threads "$THREADS" ${ASSEMBLIES}/inputs/raven_${j}.fastq.gz \
+    raven --threads "$THREADS" ${CUR_FASTQ} \
 	  --graphical-fragment-assembly ${ASSEMBLIES}/raven_${j}.gfa \
 	  > ${ASSEMBLIES}/raven_${j}.fna
     rm raven.cereal
 
 done
-
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
 
 # ------------------------------------------------------------------------
 # necat
@@ -162,19 +162,19 @@ for j in $(seq 0 $[ $NUM_NECAT_ASSEMBLIES - 1 ]) ; do
 	echo "# necat $j - already exists"
 	continue
     fi
-    echo "# necat $j"
-    make_subsample necat $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# necat $j: ${CUR_FASTQ}"
     
     echo "## running necat"
     ${PIPELINE}/scripts/run-necat -H \
 			-g "$GENOME_SIZE" -t "$THREADS" \
 			-w ${ASSEMBLIES}/tmp/necat_${j} \
 			-o ${ASSEMBLIES}/necat_${j}.fna \
-			${ASSEMBLIES}/inputs/necat_${j}.fastq.gz
+			${CUR_FASTQ}
 
 done
-
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
 
 # ------------------------------------------------------------------------
 # redbean
@@ -186,19 +186,19 @@ for j in $(seq 0 $[ $NUM_REDBEAN_ASSEMBLIES - 1 ]) ; do
 	echo "# redbean $j - already exists"
 	continue
     fi
-    echo "# redbean $j"
-    make_subsample redbean $j
+
+    CUR_SUBSAMPLE=$[ ${CUR_SUBSAMPLE} + 1 ]
+    CUR_FASTQ=$(printf "${ASSEMBLIES}/inputs/sample_%02d.fastq" ${CUR_SUBSAMPLE})
+    echo "# redbean $j: ${CUR_FASTQ}"
     
     echo "## running redbean"
     wtdbg2.pl -o ${ASSEMBLIES}/tmp/wtdbg2_${j} \
 	      -g "$GENOME_SIZE" -t "$THREADS" -x ont \
-	      ${ASSEMBLIES}/inputs/redbean_${j}.fastq.gz
+	      ${CUR_FASTQ}
     cp ${ASSEMBLIES}/tmp/wtdbg2_${j}.cns.fa \
        ${ASSEMBLIES}/redbean_${j}.fna
 
 done
-
-SEED_BASE=$[ $SEED_BASE + $SEED_BASE_SKIP ]
 
 # ------------------------------------------------------------------------
 # Done.
